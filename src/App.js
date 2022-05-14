d4fcd2b118cf2bb52c15ab811d32c3c6abc502d6
sampete1997 <sohelpatel620@gmail.com>
2022-05-14 17:39:00 +0530

    added homepage and nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-
+import HomePage from './Components/HomePage/index';
+import NavBar from './Components/NavBar';
 function App() {
   return (
+
     <div className="App">
-      <nav></nav>
+      <NavBar />
+      <HomePage />
     </div>
+
   );
 }
 
