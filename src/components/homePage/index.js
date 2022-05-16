8def78f44267e08a3e8e5e35c2e023594ffbc015
sampete1997 <sohelpatel620@gmail.com>
2022-05-16 18:10:17 +0530

    added beetpage andlocation details

--- src/components/homePage/index.js
+++ src/components/homePage/index.js
@@ -4,31 +4,56 @@ export default function HomePage() {
 
     return (
         <div>
-        <div className="homePage1">
-          
+            <div className="homePage1">
+                <div className='insideHomePage1'>
+                    <div className='reservationLogo' >
 
-        </div>
-
-
-        <div className='homePage2'>
-
-        <div className='insideHomepage2'>
-            <div>
-                    <p className='menuPara'>Menu</p >
-                    <p className='eventsPara'>Events</p >
-                    <p className='reservationPara'>Reservation</p >
                     </div>
-        </div>
+                    <p>Join us for indoor or outdoor Dinning</p>
+                </div>
+            </div>
 
-        </div>
+            <div className='homePage2'>
+
+                <div className='insideHomepage2'>
+                    <div>
+                        <p className='menuPara'>Menu</p >
+                        <p className='eventsPara'>Events</p >
+                        <p className='reservationPara'>Reservation</p >
+                    </div>
+                </div>
+
+            </div>
 
 
-        <div className='homePage3'>
-            <p className='homePage3Para'>
-            Located in the robust hills of Rancho Mission Viejo, Scarlet Kitchen & Lounge delivers the luxe and allure of old Hollywood through its elegant decor and inviting atmosphere.
-            </p>
+            <div className='homePage3'>
+                <p className='homePage3Para'>
+                    Located in the robust hills of Rancho Mission Viejo, Scarlet Kitchen & Lounge delivers the luxe and allure of old Hollywood through its elegant decor and inviting atmosphere.
+                </p>
+                <p className='homePage3Para2'>Executive chef-owner Paige Riordan masterfully blends East Coast classics with West Coast flavors.</p>
+                <button className='reservationBtn'>Make  &nbsp; a &nbsp;  Reservation</button>
+            </div>
+
+
+            <div className='beetContainer'>
+
+                <div className='beetPara'>
+                    <p>
+                        Scarlet Kitchen and Lounge is a premier kitchen and lounge serving freshly made seasonal comfort food prepared with local and quality ingredients by Executive Chef Paige Riordan and Chef Marty Manley.
+                    </p>
+                    <br />
+                    <p className='beetPara2'>
+                        This father daughter duo believe in elevated comfort food dishes that ignite diners senses and brings them to a happy place..
+                    </p>
+
+                    <p className='beetPara3'>Learn more about our story&nbsp;.</p>
+                </div>
+
+                <div className='beetImg'>
+
+                </div>
+            </div>
         </div>
-</div>
 
     )
 }
