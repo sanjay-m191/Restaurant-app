55a6ac6d20397664a968bf516564d1954541ad3e
sanjay <sanjay.m.19.1@mountblue.tech>
2022-05-16 18:29:44 +0530

    Added sign up page register new user feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/navBar';
 import HomePage from './components/homePage';
+import Login from './components/login';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <HomePage />
+      <Login />
     </div>
   );
 }
