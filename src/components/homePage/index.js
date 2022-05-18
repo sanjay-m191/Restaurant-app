81005ebfda9ab61096c2c809e6be29cd43bd0614
sampete1997 <sohelpatel620@gmail.com>
2022-05-18 10:46:32 +0530

    added newsletter container, timetable and footer

--- src/components/homePage/index.js
+++ src/components/homePage/index.js
@@ -65,11 +65,92 @@ export default function HomePage() {
                 </div>
                 <div className='drinkInfo'>
 
-                &nbsp; Scarlet Kitchen and Lounge celebrates classic speakeasy era cocktails served in elegant, vintage-style glassware as well as an all-California beer and wine list.
+                    &nbsp; Scarlet Kitchen and Lounge celebrates classic speakeasy era cocktails served in elegant, vintage-style glassware as well as an all-California beer and wine list.
                 </div>
             </div>
 
             <div className='TimeTableContainer'>
+                <div className='placeContainer'>
+                    <p> 30865 Gateway Place</p>
+                    <p> Rancho Mission Viejo, California 92694</p>
+                </div>
+
+                <button className='reservationBtn'>Get  &nbsp;  Directions</button>
+
+                <div className='timeSheet'>
+
+                    <h2>Hours</h2>
+
+                    <p>Closed Monday</p>
+
+                    <p>Tuesday-Thursday</p>
+
+                    <div>
+                        <p>Kitchen 4pm-8:30pm </p>
+                        <p> Bar 4pm-10:00pm</p>
+                    </div>
+
+                    <p> Friday & Saturday</p>
+
+                    <div>
+                        <p>Kitchen 4pm-9:30pm </p>
+                        <p> Bar 4pm-11:00pm </p>
+                    </div>
+
+                    <p> Sunday Brunch </p>
+
+                    <div className='timeSheetPara'>
+                        <p>Kitchen 10:30am-2:30pm </p>
+                        <p >Bar 10:30am-3pm</p>
+                    </div>
+
+                    <p>Sunday Dinner </p>
+
+                    <div>
+                        <p> Kitchen 5pm-8:30pm</p>
+                        <p>Bar 5pm-10:00pm</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='newsLetterContainer'>
+                <div className='newsLetter'>
+                    <p className='newsLetterPara1'>
+                        Subscribe to Our Newsletter
+                    </p>
+
+                    <p className='newsLetterPara2'>Sign up to receive news and updates.</p>
+
+                    <div className='mailcontainer'>
+                        <input className='mailBox' type={'text'} placeholder={' Email'}></input>
+                        <button className='signUpBtn'>Sign Up</button>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className='footerContainer'>
+                <div className='subfooter1'>
+                    <p className='subfooter1Para1'>Scarlet Kitchen & Lounge</p>
+                </div>
+                <div className='subfooter2'>
+                    <p>30865 Gateway Place
+                        Rancho Mission Viejo, CA 92694</p>
+                </div>
+
+                <div className='subfooter3'>
+                    <p>
+                        general inquiries email us at:
+                    </p>
+                </div>
+
+
+                <div className='subfooter4'>
+                    <p>
+                        logo
+                    </p>
+                </div>
 
             </div>
         </div>
